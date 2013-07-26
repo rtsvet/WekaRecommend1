da29fb0011646fb31aee0f89d982cd0f6bb1fb4e
gery <gery@rc-r.my>
2013-07-26 17:06:52 +0100

    initial commit

---
+++ src/wekarecommend1/WekaRecommend1.java
@@ -0,0 +1,52 @@
+package wekarecommend1;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import weka.classifiers.Classifier;
+import weka.classifiers.Evaluation;
+import weka.classifiers.functions.SMO;
+import weka.classifiers.trees.J48;
+import weka.classifiers.lazy.IB1;
+import weka.classifiers.lazy.IBk;
+import weka.clusterers.ClusterEvaluation;
+import weka.clusterers.EM;
+import weka.core.Attribute;
+import weka.core.Debug;
+import weka.core.FastVector;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.Remove;
+import weka.filters.unsupervised.attribute.StringToWordVector;
+
+public class WekaRecommend1 {
+
+    private static final String DATAFILE = "pref-train.arff";
+    private static final String TESTFILE = "pref-test.arff";
+
+    public static void main(final String[] args) throws Exception {
+        System.out.println("Running");
+
+        // Read Train Data
+        Instances data = new Instances(new BufferedReader(new FileReader(DATAFILE)));
+        data.setClassIndex(data.numAttributes() - 1);
+
+        // Read Test Data
+        Instances dataTest = new Instances(new BufferedReader(new FileReader(TESTFILE)));
+        dataTest.setClassIndex(dataTest.numAttributes() - 1);
+        // train clasiffier
+        final Classifier cf = new SMO();
+        cf.buildClassifier(data);
+
+        // make predictions
+        for (int i = 0; i < dataTest.numInstances(); i++) {
+            double pred = cf.classifyInstance(dataTest.instance(i));
+            System.out.print("Prod_ID: " + dataTest.instance(i).value(1));
+            System.out.print(", actual: " + dataTest.instance(i).value(2));
+            System.out.println(", predicted: " +  pred);
+        }
+
+
+
+    }
+}
